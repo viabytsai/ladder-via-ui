16ac1ca9031bc6dbf924bcfebf8e55c496cba583
tsai <viabytsai@gmail.com>
2022-02-25 12:04:47 +0800

    创建了我的第一个组件

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>123</div>
+  <Tsai/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Tsai from  './components/Tsai.vue'
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld 缩写 实际上是HelloWorld:HelloWorld
+    Tsai
+
+  }
+}
+</script>
