b267f433ec17fb68bb1295afede270eebe3e4d2d
tsai <viabytsai@gmail.com>
2022-02-26 11:12:50 +0800

    给button组件添加theme属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,6 +1,9 @@
 <template>
 <div>
 <Button>123</Button>
+<Button theme="button">123</Button>
+<Button theme="link">123</Button>
+<Button theme="text">123</Button>
 </div>
 </template>
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,11 +1,20 @@
 <template>
-<button>
+<button class="ladder-button"
+        :class="`theme-${theme}`"
+>
   <slot></slot>
 </button>
 </template>
 
 <script lang='ts'>
-
+export default {
+  props:{
+    theme:{
+      type:String,
+      default:'button'
+    },
+  }
+}
 </script>
 
 <style lang='scss' scoped>
