3a55d8e3ee90608cc9798b713a675f0772e1f541
tsai <viabytsai@gmail.com>
2022-02-26 17:11:45 +0800

    创建dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,9 +1,12 @@
 <template>
-<div>Dialog</div>
+<Dialog></Dialog>
 </template>
 
 <script lang='ts'>
-
+import Dialog from '../lib/Dialog.vue'
+export default {
+  components:{Dialog}
+}
 </script>
 
 <style lang='scss' scoped>
