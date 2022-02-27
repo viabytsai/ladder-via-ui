5cdaeb956ef9083825868c4a00673ff9815ed3d7
tsai <viabytsai@gmail.com>
2022-02-27 16:42:51 +0800

    实现动态设置小横条的宽度和小横条的位置

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="ladder-tabs">
-    <div class="ladder-tabs-nav">
-      <div class="ladder-tabs-nav-item "
+    <div class="ladder-tabs-nav" ref="container">
+      <div :ref="el => { if (el) navItems[index] = el }"
+            class="ladder-tabs-nav-item "
            :class="{selected:t===selected}"
            v-for="(t,index) in titles" :key="index"
            @click="select(t)">{{ t }}
       </div>
-      <div class="ladder-tabs-nav-indicator"></div>
+      <div ref="indicator"
+           class="ladder-tabs-nav-indicator"></div>
     </div>
     <div class="ladder-tabs-content">
       <component class="ladder-tabs-content-item"
@@ -19,6 +21,7 @@
 
 <script lang='ts'>
 import Tab from './Tab.vue';
+import {onMounted, onUpdated, ref} from 'vue';
 
 export default {
   props: {
@@ -27,6 +30,10 @@ export default {
     }
   },
   setup(props, context) {
+    const navItems = ref < HTMLDivElement[] > ([]);
+    const indicator = ref < HTMLDivElement > (null);
+    const container = ref < HTMLDivElement > (null)
+
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
@@ -38,8 +45,20 @@ export default {
     });
     const select = (title:string)=>{
       context.emit('update:selected',title)
+    };
+    const x =()=>{
+      const divs = navItems.value
+      const result = divs.filter(div => div.classList.contains('selected'))[0]
+      const {width} = result.getBoundingClientRect()
+      indicator.value.style.width=width+'px' //动态设置小横条的位置，实现小横条的宽度和导航item一样宽
+      const {left:left1}=container.value.getBoundingClientRect();
+      const {left:left2}=result.getBoundingClientRect();
+      const left = left2-left1;
+      indicator.value.style.left=left +'px' //动态设置小横条的宽度，实现小横条在点击不同的导航item后跳动到不同的导航item
     }
-    return {defaults, titles,select};
+    onMounted(x);
+    onUpdated(x);
+    return {container,indicator,navItems,defaults, titles,select};
   }
 };
 </script>
