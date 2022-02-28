1681c3f1c71023a1691ed122e801088327323a47
tsai <viabytsai@gmail.com>
2022-02-28 16:12:52 +0800

    创建路由

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,14 @@
 import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
+import Install from './views/Install.vue';
+import GetStarted from './views/GetStarted.vue';
+import Intro from './views/Intro.vue';
 import DocDemo from './components/DocDemo.vue';
 import SwitchDemo from './components/SwitchDemo.vue';
 import ButtonDemo from './components/ButtonDemo.vue';
 import DialogDemo from './components/DialogDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
+
 import {createRouter, createWebHashHistory} from 'vue-router';
 
 const history = createWebHashHistory();
@@ -15,6 +19,9 @@ export const router = createRouter({
         {
             path: '/doc', component: Doc, children: [
                 {path: '', component: DocDemo},
+                {path: 'install', component: Install},
+                {path: 'intro', component: Intro},
+                {path: 'get-started', component: GetStarted},
                 {path: 'switch', component: SwitchDemo},
                 {path: 'button', component: ButtonDemo},
                 {path: 'dialog', component: DialogDemo},
---
+++ src/views/Intro.vue
@@ -0,0 +1,11 @@
+<template>
+<div>介绍</div>
+</template>
+
+<script lang='ts'>
+
+</script>
+
+<style lang='scss' scoped>
+
+</style>
